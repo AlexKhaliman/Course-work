e39068cb33ca0c81be5a0227bf3b04c2c3d83b7d
AlexKhaliman <khalimantsevich174@mail.ru>
2019-09-30 10:49:30 +0300

    added authentication

--- actions/urls.py
+++ actions/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('creating/', views.create_task, name='create_task'),
     path('login/', views.login_view, name='login'),
+    path('', views.welcome, name="welcome"),
+    path('logout/', views.logout_view, name="logout"),
+    path('register/', views.register, name="register"),
+    path('account/', views.account, name="account")
 ]
 
--- actions/views.py
+++ actions/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
@@ -6,5 +7,40 @@ def create_task(request):
     return HttpResponse('Здесь должна быть форма заполнения')
 
 
+def welcome(request):
+    return render(request, 'actions/welcome.html')
+
+
 def login_view(request):
-    return render(request, 'actions/login.html')
+    if request.method == 'GET':
+        return render(request, "actions/login.html", context={
+            "error": False
+        })
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(
+            username=username,
+            password=password
+        )
+        if user is not None:
+            login(request, user)
+            print(request.GET.get('next'))
+            return redirect('/')
+        else:
+            return render(request, "actions/login.html", context={
+                "error": True
+            })
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
+
+
+def register(request):
+    return HttpResponse('Здесь должная быть форма регистрации')
+
+
+def account(request):
+    return HttpResponse('личный кабинет')
